e08fb6232c2affc3f721576b24adc0420457c286
Akhil Repala <arepala@blinklabs.io>
2025-11-02 21:36:44 -0600

    feat(bursa): Fixed the extraction part of private key & public key for decodeExtendedCborKey
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>

--- bursa.go
+++ bursa.go
@@ -339,7 +339,11 @@ func decodeExtendedCborKey(skeyBytes []byte) ([]byte, []byte, error) {
 	if skeyBytes[0] != 0x58 || skeyBytes[1] != 0x80 {
 		return nil, nil, errors.New("invalid cbor skey hex prefix")
 	}
-	return skeyBytes[2:98], skeyBytes[66:98], nil
+	epk := skeyBytes[2:66]
+	chainc := skeyBytes[98:130]
+	skey := append(epk, chainc...)
+
+	return skey, skeyBytes[66:98], nil
 }
 
 func decodeVerificationKey(vkeyBytes []byte) ([]byte, error) {
